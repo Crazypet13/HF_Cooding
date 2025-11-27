5aebd020078eb8c8d5666c5b423f0cf445465536
Peter Magyar <magyar13peter13@gmail.com>
2025-11-27 14:23:18 +0100

    geting back in the loop

---
+++ 25_11_27.js
@@ -0,0 +1,19 @@
+/*Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+    Sort numbers array in ascending order.
+    If the function's argument is null, an empty array, or undefined; return an empty array.
+    Return a new array of sorted numbers.
+*/
+
+let numbers = Array [1,2,3,52,32,8,16,4265,252];
+
+function sorter (input_array){
+    if(input_array.length()===0 || input_array.type() === NaN || input_array.type() === null){
+        return let = []; 
+    }else{
+        console.log(input_array.sort());
+    }
+    
+};
+
+sorter(numbers);
