4fc5d05cf14dacbb38c95fedf69745f86e04d0ee
Peter Magyar <magyar13peter13@gmail.com>
2025-11-27 18:31:18 +0100

    middle of a project2

--- 25_11_27.js
+++ 25_11_27.js
@@ -3,7 +3,7 @@
     Sort numbers array in ascending order.
     If the function's argument is null, an empty array, or undefined; return an empty array.
     Return a new array of sorted numbers.
-*/
+---------------------------------------------------------------------------------------------------
 
 let numbers = [1,2,3,52,32,8,16,4265,252,-52,-1];
 
@@ -17,3 +17,18 @@ function sorter (input_array){
 };
 
 sorter(numbers);
+
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+*/
+// for loop with if else(or elvis what is less line) in it so you check for all the type of the arrays objects if its not number ,and dont add it in a returning value
+// using array filter give it so only return is its a number and || bigger then 0 *** 
+let box = [1, 2, "aasf", "1", "123", 123];
+
+only_positive = (input_arrey) => {
+    input_arrey.filter(()=>(value),value.typeof === Number);
+}
+/*gpt sayso :/ dont working to
+ const getNumbers = arr =>
+  arr.filter(item => typeof item === "number" && !isNaN(item));
+*/
+console.log(only_positive(box));
